3ebce8e41cff1276a3de1efbdb4755429b12c65d
Sanya <sashaklgn@gmail.com>
2021-12-23 16:07:43 +0400

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    public Person person;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void display () {
+        System.out.println(person.id  + " " + person.name + " " + person.age);
+    }
+}
